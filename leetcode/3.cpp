8caf0d42074468ea26f3a2c20e83b0a8b8ed7574
leebohee <bohee46@gmail.com>
2019-03-15 17:55:30 +0900

    Update 3.cpp
    Improve performance

--- leetcode/3.cpp
+++ leetcode/3.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int sublen = 0, len = s.length(), start = 0, end = 0;
-        unordered_set <char> us;
+        unordered_map <char, int> um;
         
         while(end < len){
-            if(us.find(s.at(end)) != us.end()){ // repeated character
-                if(sublen < us.size()){
-                    sublen = us.size();
-                }
-                while(s.at(start) != s.at(end)){
-                    us.erase(s.at(start++));
-                }
-                start++;
+            auto itr = um.find(s.at(end));
+            if(itr != um.end()){ // repeated character
+                sublen = max(sublen, end-start);
+                start = max((itr->second)+1, start);
             }
-            else{
-                us.insert(s.at(end));
-            }
+            um[s.at(end)] = end;
             end++;
         }
         
-        return (sublen > us.size())? sublen : us.size();
+        return max(sublen, end-start);
     }
 };
