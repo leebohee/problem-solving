93a4c597c06e90751c97d854f29dce6adeaa81d2
leebohee <bohee46@gmail.com>
2019-03-25 16:18:35 +0900

    Update 942.cpp
    Another implementation

--- leetcode/942.cpp
+++ leetcode/942.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
     vector<int> diStringMatch(string S) {
-        int len = S.length(), cntD = 0, j;
+        int len = S.length(), low = 0, high = len;
         vector <int> A;
-        bool chk[len+1] = {false};
-        
-        // Count the number of D
-        for(int i=0; i<len; i++){
-            if(S.at(i) == 'D') cntD++;
-        }
-        // Set A[0] as the number of D
-        A.push_back(cntD);
-        chk[A[0]] = true;
         
         for(int i=0; i<len; i++){
             if(S.at(i) == 'I'){
-                j = 1;
-                while(chk[A[i]+j]){
-                    j++;
-                }
-                A.push_back(A[i]+j);
+                A.push_back(low++);
             }
             else{
-                j = 1;
-                while(chk[A[i]-j]){
-                    j++;
-                }
-                A.push_back(A[i]-j);
+                A.push_back(high--);
             }
-            chk[A[i+1]] = true;
         }
+        A.push_back(low);
         
         return A;
     }
