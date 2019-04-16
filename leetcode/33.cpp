01cd46e833e8b0bf7875518e95f38cfa9fddc00c
leebohee <bohee46@gmail.com>
2019-04-16 14:27:34 +0900

    Update 33.cpp

--- leetcode/33.cpp
+++ leetcode/33.cpp
@@ -4,45 +4,33 @@ public:
         int cnt = nums.size();
         int left = 0, right = cnt-1, mid;
         
+        if(cnt == 0) return -1;
+        
         while(left <= right){
             mid = (left+right)/2;
+            
             if(nums[mid] == target){
                 return mid;
             }
             
-            if(nums[left] < nums[right]){ // ordered range
-                if(nums[mid] > target){
+            if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && target < nums[mid]){
                     right = mid - 1;
                 }
                 else{
                     left = mid + 1;
                 }
             }
-            else{ // unordered range
-                if(nums[mid] < target){
-                    if(nums[left] < nums[mid]){
-                        left = mid + 1;
-                    }
-                    else if(nums[right] < target){
-                        right = mid - 1;
-                    }
-                    else{
-                        left = mid + 1;
-                    }
+            else{ // nums[mid] < nums[right]
+                if(nums[mid] < target && target <= nums[right]){
+                    left = mid + 1;
                 }
                 else{
-                    if(nums[left] > nums[mid]){
-                        right = mid - 1;
-                    }
-                    else if(nums[left] <= target){
-                        right = mid - 1;
-                    }
-                    else{
-                        left = mid + 1;
-                    }
+                    right = mid - 1;
                 }
             }
         }
+        
         return -1;
     }
 };
