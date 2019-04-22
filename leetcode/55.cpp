b3a2954efcd6c3d8067d68d918f1b9bdf923afac
leebohee <bohee46@gmail.com>
2019-04-22 15:05:57 +0900

    Update 55.cpp
    Bottom-up implementation

--- leetcode/55.cpp
+++ leetcode/55.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    bool canReach(vector <int> &nums, int pos, int memo[]){
-        if(pos == nums.size() - 1){
-            return true;
-        }
-        if(memo[pos] == -1){
-            int jump_len = nums[pos];
-            bool result = false;
-            for(int i=1; i<=jump_len; i++){
-                result = result | canReach(nums, pos+i, memo);
-                if(result){
-                    memo[pos] = 1;
-                    return true;
-                }
-            }
-            memo[pos] = 0;
-        }
-        return memo[pos];
-    } 
-    
     bool canJump(vector<int>& nums) {
         int size = nums.size();
-        int* memo = new int [size];  // -1: not visited, 0: cannot reach, 1: can reach
-        memset(memo, -1, sizeof(int)*size);
-        return canReach(nums, 0, memo);
+        char* memo = new char [size];  // U: not visited, F: cannot reach, T: can reach
+        memset(memo, 'U', sizeof(char)*size);
+        memo[size-1] = 'T';
+        
+        for(int i=size-2; i>=0; i--){
+            int max_jump = min(size-1, i+nums[i]);
+            for(int j=max_jump; j>0; j--){
+                if(memo[j] == 'T'){
+                    memo[i] = 'T';
+                    break;
+                }
+            }
+        }
+        return (memo[0] == 'T');
     }
 };
