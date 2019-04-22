d66fa96f67df85b54b4f578f0950a98323a18fde
leebohee <bohee46@gmail.com>
2019-04-22 15:08:07 +0900

    Update 55.cpp
    Improve performance by greedy algorithm

--- leetcode/55.cpp
+++ leetcode/55.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int size = nums.size();
-        char* memo = new char [size];  // U: not visited, F: cannot reach, T: can reach
-        memset(memo, 'U', sizeof(char)*size);
-        memo[size-1] = 'T';
+        int lastPossible = size - 1;
         
         for(int i=size-2; i>=0; i--){
-            int max_jump = min(size-1, i+nums[i]);
-            for(int j=max_jump; j>0; j--){
-                if(memo[j] == 'T'){
-                    memo[i] = 'T';
-                    break;
-                }
+            if(i+nums[i] >= lastPossible){
+                lastPossible = i;
             }
         }
-        return (memo[0] == 'T');
+        return (lastPossible == 0);
     }
 };
