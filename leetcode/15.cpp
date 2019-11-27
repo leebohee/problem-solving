ec6726994dbba855807d241c2c53f9cc53e181de
Lee Bohee <bohee46@gmail.com>
2019-11-27 20:01:31 +0900

    Update 15.cpp
    Without unordered_set

--- leetcode/15.cpp
+++ leetcode/15.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int size = nums.size();
-        int target;
+        int n = nums.size();
+        // sort nums
+        sort(nums.begin(), nums.end());
+
         vector < vector <int> > result;
-        vector <int> tmp_result;
-        unordered_multimap <int, int> us;
-        
-        sort(nums.begin(), nums.end()); // O(NlogN)
-        
-        for(int i=0; i<size; i++){
-            us.insert(make_pair(nums[i], i));
+        vector <int> triplet;
+        for(int k=0; k<n; ){
+        	int target = nums[k];
+        	int i = k+1, j = n-1;
+        	while(i < j){
+        		int sum = nums[i] + nums[j];
+        		if(-1*target < sum){
+        			j--;
+        		}
+        		else if(-1*target > sum){
+        			i++;
+        		}
+        		else{
+		        	triplet.push_back(nums[i]);
+		        	triplet.push_back(nums[j]);
+		        	triplet.push_back(-1*sum);
+		        	result.push_back(triplet);
+		        	triplet.clear();
+	        		int a = nums[i], b = nums[j];
+	        		while(i < j && nums[i] == a) i++;        	
+	        		while(i < j && nums[j] == b) j--;
+        		}
+        	}
+        	while(k < n && nums[k] == target) k++;
         }
-        for(int i=0; i<size; i++){
-            for(int j=i+1; j<size; j++){
-                target = -1 * (nums[i] + nums[j]);
-                auto itr = us.find(target);
-                if(itr != us.end() && (itr->second > j)){ // exist
-                    tmp_result.push_back(nums[i]);
-                    tmp_result.push_back(target);
-                    tmp_result.push_back(nums[j]);
-                    result.push_back(tmp_result);
-                    tmp_result.clear();
-                }
-                // skip duplicate
-                while(j < size-1 && nums[j] == nums[j+1]){
-                    j++;
-                }
-            }
-            // skip duplicate
-            while(i < size-1 && nums[i] == nums[i+1]){
-                i++;
-            }
-        }
-        
         return result;
     }
 };
