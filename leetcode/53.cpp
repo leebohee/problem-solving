a701dc89d4f210a23114f9747c6ded6c522ddf94
Lee Bohee <bohee46@gmail.com>
2020-07-03 18:42:35 +0900

    Implement with Divide-and-Conquer

--- leetcode/53.cpp
+++ leetcode/53.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_DP {
 public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size(), max;
@@ -11,5 +11,32 @@ public:
             if(max < memo[i]) max = memo[i];
         }
         return max;
+    }
+};
+
+class Solution_D {
+public:
+    struct ret{
+        int lsum;
+        int rsum;
+        int subsum;
+        int totalsum;
+        ret(int l, int r, int s, int t): lsum(l), rsum(r), subsum(s), totalsum(t) {};
+    };
+    ret max_subarray(vector<int>& nums, int start, int end){
+        if(start == end) return ret(nums[start], nums[start], nums[start], nums[start]);
+        int mid = start + (end - start) / 2;
+        ret left = max_subarray(nums, start, mid);
+        ret right = max_subarray(nums, mid+1, end);
+        
+        int l = max(left.lsum, left.totalsum + right.lsum);
+        int r = max(right.rsum, right.totalsum + left.rsum);
+        int s = max(max(left.subsum, right.subsum), left.rsum + right.lsum);
+        int t = left.totalsum + right.totalsum;
+        
+        return ret(l, r, s, t);
+    }
+    int maxSubArray(vector<int>& nums) {
+        return max_subarray(nums, 0, nums.size()-1).subsum;
     }
 };
