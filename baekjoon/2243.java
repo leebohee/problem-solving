6a04eb47e5a33ab9081eb0d2b70cd4dfd0bdf727
Lee Bohee <bohee46@gmail.com>
2020-08-13 23:00:41 +0900

    Complete problem #2243

--- baekjoon/2243.java
+++ baekjoon/2243.java
@@ -10,36 +10,49 @@ import java.util.StringTokenizer;
 public class Main {
 	static int n;
 	static int[] candies; // candy[i]: 맛이 i인 사탕 개수
+
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		System.setIn(new FileInputStream("src\\Day03\\P2243\\input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		n = Integer.parseInt(st.nextToken());
-		candies = new int [1000001];
-		
+		candies = new int[1000001];
+
 		IndexedTree it = new IndexedTree(candies);
 		it.makeTree(1, 1, it.leafSize);
-		System.out.println(Integer.toString(it.leafSize));
-		
-		//// 순위가 B인 사탕 구하기 == binary search로 candy[1-k]=B인 lower bound k를 구한다.
-		
-		// 순위가 B인 사탕 구하기 == indexed tree에서 B를 쿼리하는데, 노드의 값(개수누적합)과 B를 비교해서
-		// 마지막 레벨까지 찾아감	
-		/* 자신의 왼쪽 자식의 값과 쿼리의 값을 비교해서, 
-		 * 왼쪽 자식 값이 크거 같으면 왼쪽으로 (순위)로 쿼리
-		 * 왼쪽 자식 값이 더 작으면 오른쪽으로 (순위-왼쪽 자식값)으로 쿼리
-		 *     4
-		 *    / \
-		 *   1   3
-		 *   
-		 *   에서 2위를 찾고 싶으면 오른쪽 자식노드에게 (2-1)위를 쿼리를 날림
-		 *   만약 1위를 찾고 싶었으면 왼쪽 자식
-		 */
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+
+			if (A == 1) { // 순위가 B인 사탕 꺼내기
+				// 순위가 B인 사탕 구하기 == indexed tree에서 B를 쿼리하는데, 노드의 값(개수누적합)과 B를 비교해서
+				// 마지막 레벨까지 찾아감
+				/*
+				 * 자신의 왼쪽 자식의 값과 쿼리의 값을 비교해서, 왼쪽 자식 값이 크거 같으면 왼쪽으로 (순위)로 쿼리 왼쪽 자식 값이 더 작으면 오른쪽으로
+				 * (순위-왼쪽 자식값)으로 쿼리 
+				 *   4
+				 *  / \ 
+				 * 1   3
+				 * 
+				 * 에서 2위를 찾고 싶으면 오른쪽 자식노드에게 (2-1)위를 쿼리를 날림 만약 1위를 찾고 싶었으면 왼쪽 자식
+				 */
+				int flavor = it.query(1, 1, it.leafSize, B);
+				it.update(1, 1, it.leafSize, flavor, -1);
+				System.out.println(flavor);
+			} else { // 맛이 B인 사탕 C개 추가하기
+				int C = Integer.parseInt(st.nextToken());
+				it.update(1, 1, it.leafSize, B, C);
+			}
+		}
+
 	}
 
 }
+
 class IndexedTree {
 	int[] tree; // indexed tree
 	int[] nums; // 실제 값
@@ -62,7 +75,7 @@ class IndexedTree {
 	public int makeTree(int node, int left, int right) {
 		// leaf에 도달하면 데이터를 채워준다.
 		if (left == right) {
-			if (left <= nums.length) {
+			if (left < nums.length) {
 				return tree[node] = nums[left];
 			} else { // 여유 공간 => 합에 영향을 미치지 않게 0으로 초기화
 				return tree[node] = 0;
@@ -75,34 +88,28 @@ class IndexedTree {
 		tree[node] = makeTree(node * 2, left, mid);
 		tree[node] += makeTree(node * 2 + 1, mid + 1, right);
 		return tree[node];
+
 	}
 
-	// 원하는 구간의 합 or 문제의 답을 구한다.
-	// left: 노드의 구간 시작 값, right: 노드의 구간 끝 값
-	// qLeft: 쿼리의 구간 시작 값, qRight: 쿼리의 구간 끝 값
-	public int query(int node, int left, int right, int qLeft, int qRight) {
-		if (qRight < left || right < qLeft) { // 관련 없는 경우
-			return 0; // 바로 리턴하니까 시간복잡도 계산에 고려하지 않아도 됨
-		}
-		else if(qLeft <= left && right <= qRight) { // 쿼리에 완전 속하는 경우
-			return tree[node];
-		}
-		else { // 그 외 (애매하게 걸쳐있는 경우)
-			int mid = left + (right - left) / 2;
-			return query(node*2, left, mid, qLeft, qRight) 
-					+ query(node*2+1, mid+1, right, qLeft, qRight);
+	public int query(int node, int left, int right, int rank) {
+		if (left == right) return left;
+		int mid = left + (right - left) / 2;
+		if (tree[node * 2] >= rank) {
+			return query(node * 2, left, mid, rank);
+		} else {
+			return query(node * 2 + 1, mid + 1, right, rank - tree[node * 2]);
+
 		}
 	}
 
 	// 특정 값을 갱신한다.
 	public void update(int node, int left, int right, int index, int diff) {
-		if(index < left || right < index) {
+		if (index < left || right < index) {
 			return;
-		}
-		else {
+		} else {
 			tree[node] += diff;
-			if(left != right) { // 내부 노드 (leaf에 도달할 때까지 재귀적으로 업데이트)
-				int mid = (left + right) / 2;
+			if (left != right) { // 내부 노드 (leaf에 도달할 때까지 재귀적으로 업데이트)
+				int mid = left + (right - left) / 2;
 				update(node * 2, left, mid, index, diff);
 				update(node * 2 + 1, mid + 1, right, index, diff);
 			}
